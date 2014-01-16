9c9c11c4700bd9b3116e0f5543d71378ae44d21c
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2014-01-16 10:04:45 +0000

    Vanilla commit of Dexterity product to kick-off development

---
+++ README.txt
@@ -0,0 +1,6 @@
+Introduction
+============
+
+
+
+Enid Linder Foundation application form.
---
+++ enidlinder/app/tests.py
@@ -0,0 +1,58 @@
+import doctest
+import unittest
+
+from Testing import ZopeTestCase as ztc
+
+from Products.Five import zcml
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import PloneSite
+from Products.PloneTestCase.layer import onsetup
+
+import enidlinder.app
+
+OPTION_FLAGS = doctest.NORMALIZE_WHITESPACE | \
+               doctest.ELLIPSIS
+
+ptc.setupPloneSite(products=['enidlinder.app'])
+
+
+class TestCase(ptc.PloneTestCase):
+
+    class layer(PloneSite):
+
+        @classmethod
+        def setUp(cls):
+            zcml.load_config('configure.zcml',
+              enidlinder.app)
+
+        @classmethod
+        def tearDown(cls):
+            pass
+
+
+def test_suite():
+    return unittest.TestSuite([
+
+        # Unit tests
+        #doctestunit.DocFileSuite(
+        #    'README.txt', package='enidlinder.app',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+        #doctestunit.DocTestSuite(
+        #    module='enidlinder.app.mymodule',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+
+        # Integration tests that use PloneTestCase
+        ztc.ZopeDocFileSuite(
+            'INTEGRATION.txt',
+            package='enidlinder.app',
+            optionflags = OPTION_FLAGS,
+            test_class=TestCase),
+
+        # -*- extra stuff goes here -*-
+
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
